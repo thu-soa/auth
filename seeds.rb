58dad176a8dda1d4c2ec218cd1b584e7c8a72ee1
Alex Wang <ice_b0und@hotmail.com>
2016-05-30 13:38:15 +0800

    change seeds

--- seeds.rb
+++ seeds.rb
@@ -2,6 +2,7 @@ require './main'
 
 User.create(name: 'learn', user_type: :source)
 User.create(name: 'github', user_type: :source)
+User.create(name: 'vultr', user_type: :source)
 master = User.create(name: 'alexwang', user_type: :user)
 
 Token.create(user_id: master.id, token_string: '123')
